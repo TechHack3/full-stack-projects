baf20bb3eae0a2a7b529e42127fbda6eab1aac74
TechHack3 <140188790+TechHack3@users.noreply.github.com>
2024-10-28 21:11:39 +0530

    Revert "added new project"

--- URLshortener/routes/url.js
+++
@@ -1,7 +0,0 @@
-const express=require("express");
-const router=express.Router();
-const {generateNewShortURL}=require("../controllers/url");
-
-router.post('/',generateNewShortURL);
-
-module.exports=router;
